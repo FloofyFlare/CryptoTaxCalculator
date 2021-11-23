213ac2691591143b66d369e706fdb63074809393
Raymond Flowers IV <85596143+FloofyFlare@users.noreply.github.com>
2021-11-23 08:18:54 -0500

    Add files via upload

---
+++ CryptoTaxCalculator/PersonalInfo.java
@@ -0,0 +1,15 @@
+public class PersonalInfo
+{
+    // instance variables - replace the example below with your own
+    
+    public PersonalInfo()
+    {
+        // initialise instance variables
+        
+    }
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return 0;
+    }
+}
