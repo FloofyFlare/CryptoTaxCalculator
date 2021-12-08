a4fd0229087f2c8ddbf7c8e32c5f67997022a704
Raymond Flowers IV <85596143+FloofyFlare@users.noreply.github.com>
2021-12-08 07:50:53 -0500

    Add files via upload

--- CryptoTaxCalculator/Calculations.java
+++ CryptoTaxCalculator/Calculations.java
@@ -1,33 +1,149 @@
+// https://www.javatpoint.com/how-to-read-csv-file-in-java
+import java.io.*;  
+import java.util.Scanner;  
+// end 
 
-/**
- * Write a description of class Calculations here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class Calculations
 {
     // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Calculations
-     */
-    public Calculations()
-    {
-        // initialise instance variables
-        x = 0;
+    public static float capitalGain(float amount, float buy, float sell){
+        float awnser = (sell * amount) - (buy*amount);
+        return awnser;
+    }
+    
+    public static float shortTaxMatrix (float income) {
+        double[][] array = new double[4][7];
+        System.out.println(" How are you filing your taxes: individuals(0) married filing Joint(1) For Heads Of Households(2) INTEGERS ONLY");
+        Scanner sc = new Scanner(System.in);    
+          boolean findingLength = true;
+          String filing = " ";
+          boolean Lterm = true;
+          int x = 0;
+         while (findingLength == true)
+           {
+               filing = sc.nextLine();
+               if(filing.substring(0,1).equalsIgnoreCase("0")){
+                   findingLength = false;
+                   Lterm = true;
+                   x = 1;
+               }
+               else if (filing.substring(0,1).equalsIgnoreCase("1")){
+                   findingLength = false;
+                   Lterm = false;  
+                   x = 2;
+               }
+                else if (filing.substring(0,1).equalsIgnoreCase("2")){
+                   findingLength = false;
+                   Lterm = false;  
+                   //https://www.javatpoint.com/java-integer-parseint-method
+                   x = 3;
+               }
+               else {
+                 System.out.println("input error{ try again }: ");  
+               }
+           }
+           
+        System.out.println("What is your yearly income (NUMBERS ONLY)");
+        float yearlyIncome = sc.nextFloat();
+        
+        //rate
+        array[0][0] = 0.10; 
+        array[0][1] = 0.12;
+        array[0][2] = 0.22;
+        array[0][3] = 0.24;
+        array[0][4] = 0.32;
+        array[0][5] = 0.35;
+        array[0][6] = 0.37;
+        
+        array[1][0] = 0; 
+        array[1][1] = 9951;
+        array[1][2] = 40526;
+        array[1][3] = 86376;
+        array[1][4] = 164926;
+        array[1][5] = 209426;
+        array[1][6] = 628301;
+        
+        array[2][0] = 0; 
+        array[2][1] = 19901;
+        array[2][2] = 81051;
+        array[2][3] = 172751;
+        array[2][4] = 329851;
+        array[2][5] = 418851;
+        array[2][6] = 628301;
+        
+        array[3][0] = 0; 
+        array[3][1] = 14201;
+        array[3][2] = 54201;
+        array[3][3] = 86351;
+        array[3][4] = 164901;
+        array[3][5] = 209401;
+        array[3][6] = 523601;
+        
+        int i = 6;
+     
+        while(array[x][i] >= yearlyIncome){   
+            i--;
+        }
+        return ((float)array[0][i]);
+        
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public static float longTaxMatrix (float income) {
+        double[][] array = new double[4][7];
+        System.out.println(" How are you filing your taxes: individuals(0) married filing Joint(1) For Heads Of Households(2) INTEGERS ONLY");
+        Scanner sc = new Scanner(System.in);    
+          boolean findingLength = true;
+          String filing = " ";
+          boolean Lterm = true;
+          int x = 0;
+         while (findingLength == true)
+           {
+               filing = sc.nextLine();
+               if(filing.substring(0,1).equalsIgnoreCase("0")){
+                   findingLength = false;
+                   Lterm = true;
+                   x = 1;
+               }
+               else if (filing.substring(0,1).equalsIgnoreCase("1")){
+                   findingLength = false;
+                   Lterm = false;  
+                   x = 2;
+               }
+                else if (filing.substring(0,1).equalsIgnoreCase("2")){
+                   findingLength = false;
+                   Lterm = false;  
+                   //https://www.javatpoint.com/java-integer-parseint-method
+                   x = 3;
+               }
+               else {
+                 System.out.println("input error{ try again }: ");  
+               }
+           }
+           
+        System.out.println("What is your yearly income (NUMBERS ONLY)");
+        float yearlyIncome = sc.nextFloat();
+        
+        //rate
+        array[0][0] = 0; 
+        array[0][1] = 0.15;
+        array[0][2] = 0.20;
+        
+        array[1][0] = 0; 
+        array[1][1] = 40000;
+        array[1][2] = 441450;
+        
+        array[2][0] = 0; 
+        array[2][1] = 80000;
+        array[2][2] = 496600;
+        
+        array[3][0] = 0; 
+        array[3][1] = 53600;
+        array[3][2] = 469050;
+        
+        int i = 2;
+        while(array[x][i] >= yearlyIncome){   
+            i--;
+        }
+        return ((float)array[0][i]);
     }
 }
--- CryptoTaxCalculator/PersonalInfo.java
+++ CryptoTaxCalculator/PersonalInfo.java
@@ -1,15 +1,77 @@
+import java.util.Scanner;
+
 public class PersonalInfo
 {
-    // instance variables - replace the example below with your own
+    static Scanner sc = new Scanner(System.in);
+    static Calculations calc = new Calculations();
     
-    public PersonalInfo()
+    public static void main (String args [])
     {
-        // initialise instance variables
-        
+           System.out.println("Welcome to the Crypto Tax: Capital Gain Calculator");
+           boolean longTerm = isLongTerm();
+           float income = capitalGains();
+           taxBracket(isLongTerm(), capitalGains());
+           System.out.println("Have you paid your taxes yet");
+           
+            boolean findingLength = true; 
+            String yORn = " ";
+           while (findingLength == true)
+           {
+               yORn = sc.nextLine();
+               if(yORn.substring(0,1).equalsIgnoreCase("y")){
+                   findingLength = false;
+                   
+               }
+               else if (yORn.substring(0,1).equalsIgnoreCase("n")){
+                   System.out.println("The IRS will be there tomorrow");
+                   findingLength = false;  
+               }
+               else {
+                 System.out.println("input error{ try again }: ");  
+               }
+           }
     }
-    public int sampleMethod(int y)
+    public static float capitalGains ()
     {
-        // put your code here
-        return 0;
+        //Issue one no error handle for non floats 
+        System.out.println("Please input the asset amount");
+        float assetAmount = sc.nextFloat();
+        System.out.println("Please input the price bought at (including network fee's)");
+        float priceInt = sc.nextFloat();
+        System.out.println("Please input the price sold at");
+        float pricePost = sc.nextFloat();
+        float gain = calc.capitalGain(assetAmount, priceInt, pricePost);
+        System.out.println("capital gain $" + gain);
+        return gain;
+    } 
+    public static boolean isLongTerm () {
+        boolean findingLength = true; 
+        boolean Lterm = false;
+        System.out.println("Was the asset held longer than 1 year (y/n):");
+            String yORn = " ";
+        while (findingLength == true)
+           {
+               yORn = sc.nextLine();
+               if(yORn.substring(0,1).equalsIgnoreCase("y")){
+                   findingLength = false;
+                   Lterm = true;
+               }
+               else if (yORn.substring(0,1).equalsIgnoreCase("n")){
+                   findingLength = false;
+                   Lterm = false;  
+               }
+               else {
+                 System.out.println("input error{ try again }: ");  
+               }
+           }
+        return Lterm;
+    }
+    public static void taxBracket (boolean isLong, float cashEarned){
+        if (isLong){
+            System.out.println("Your tax owed " + (cashEarned * (calc.longTaxMatrix(cashEarned))));
+        }
+        else{
+        System.out.println("Your tax owed " + (cashEarned * (calc.shortTaxMatrix(cashEarned))));
+        }
     }
 }
